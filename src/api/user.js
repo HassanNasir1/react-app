97169580c7f6ce769ece27a3393d61ce2aa90d55
hanny <hanny.mughal@gmail.com>
2022-10-07 09:52:42 +0500

    used inceptors to handle response before then catch

--- src/api/user.js
+++ src/api/user.js
@@ -7,7 +7,7 @@ export function getAllUsers() {
     .then(function (response) {
       // handle success
 
-      return response.data;
+      return response;
     })
     .catch(function (error) {
       // handle error
@@ -18,36 +18,21 @@ export function getAllUsers() {
     });
 }
 
-export async function registerUser(name, setName,  email, setEmail, password , setPassword, userType, setUserType, setErr) {
+export function registerUser(name, email, password, userType) {
   const data = {
     name: name,
     email: email,
     password: password,
     userType: userType,
-
   };
-
-  await axios
-    .post("/register", data)
-    .then(function (response) {
-      if (response.status == 200) {
-        setName("");
-        setEmail("");
-        setPassword("");
-        setUserType("");
-
-        return response.data;
-      }
+  return axios.post("/register", data)
+    .then((response) => {
+        console.log(response);
+     return response;
     })
-    .catch(function (error) {
-      console.log(error.response.data.message);
-      setErr(
-        error.response.data.error
-          ? error.response.data.error
-          : error.response.data.message
-          ? error.response.data.message
-          : ""
-      );
+    .catch((error) => {
+    
+        return Promise.reject(error.response.data.error);
     });
 }
 
