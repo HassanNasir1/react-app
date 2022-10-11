bbba82a7d1e61efeb8490b425d27bfdb214bbff5
hanny <hanny.mughal@gmail.com>
2022-10-11 10:14:34 +0500

    display courses on student or quiz panel

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -1,41 +1,69 @@
 import Table from "../component/Table";
 import { useState, useEffect } from "react";
 import { remove, getAll } from "../api/course";
-import QuizCreator from '../component/QuizCreator';
+import { getImages } from "../api/unsplash";
+import Header from "../component/Header";
+import Footer from "../component/Footer";
+import Card from "../component/Card";
 
 const Quiz = () => {
-//   const [course, setCourse] = useState([]);
-//   function onDeleteRow(id) {
-//    return remove(id).then((response) => {
-//       const newCourses = course.filter((course) => {
-//         return course._id != id;
-//       });
-//       setCourse(newCourses);
-//       return true
-//     }).catch((err) => {
-//         return false;
-//     });
-//   }
+  const [course, setCourse] = useState([]);
+  const [images, setImages] = useState({});
 
-//   useEffect(() => {
-//     getAll()
-//       .then((response) => {
-//         console.log(response);
-//         setCourse(response);
-//         //console.log(response);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   }, []);
+  useEffect(() => {
+    getAll()
+      .then((response) => {
+        console.log(response);
+        setCourse(response);
+        //console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-//   const headings = ["Course Name", "Course Code", "Course Type", "Action"];
+  useEffect(() => {
+    getImages()
+      .then((response) => {
+        console.log("response", response);
+        setImages(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-//   const schema = ["courseName", "courseCode", "courseType"];
+  //   const [course, setCourse] = useState([]);
+  //   function onDeleteRow(id) {
+  //    return remove(id).then((response) => {
+  //       const newCourses = course.filter((course) => {
+  //         return course._id != id;
+  //       });
+  //       setCourse(newCourses);
+  //       return true
+  //     }).catch((err) => {
+  //         return false;
+  //     });
+  //   }
+
+  //   useEffect(() => {
+  //     getAll()
+  //       .then((response) => {
+  //         console.log(response);
+  //         setCourse(response);
+  //         //console.log(response);
+  //       })
+  //       .catch((error) => {
+  //         console.error(error);
+  //       });
+  //   }, []);
+
+  //   const headings = ["Course Name", "Course Code", "Course Type", "Action"];
+
+  //const schema = ["courseName", "courseCode", "courseType"];
 
   return (
     <>
-
       {/* <Table
         headings={headings}
         data={course}
@@ -43,9 +71,16 @@ const Quiz = () => {
         onDeleteRow={onDeleteRow}
         name={"Courses"}
       /> */}
+      <Header
+        heading={"Welcome to Student Panel"}
+        tagline={
+          "loremLorem ipsum is a placeholder text commonly used or a typeface without relying on meaningful content"
+        }
+      />
 
-      <QuizCreator/>
+      <Card course={course} images={images.results} />
 
+      <Footer />
     </>
   );
 };
