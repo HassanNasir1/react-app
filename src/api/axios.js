a1c31262dc5079b1e859dc8916ce4d4bdee697c7
hanny <hanny.mughal@gmail.com>
2022-10-11 10:07:11 +0500

    remove unnecessary comments

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,15 +5,11 @@ const axiosInstance = axios.create({
     headers: {'X-Custom-Header': 'foobar'}
   });
   axiosInstance.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     console.log(response)
     return response.data;
 
     
   }, function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
     console.error(error)
     return Promise.reject(error);
   });
