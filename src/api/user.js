20bd59c005f39e54db417022e23f424a2f894785
hanny <hanny.mughal@gmail.com>
2022-10-13 11:52:39 +0500

    rearranging some functions

--- src/api/user.js
+++ src/api/user.js
@@ -1,9 +1,39 @@
 import axios from "./axios";
+import { useState } from "react";
+// import Userfront from "@userfront/core";
+// Userfront.init("demo1234");
 
 // Make a request for a user with a given ID
+
+export function signInUser(email, password) {
+  const data = {
+  
+    email: email,
+    password: password,
+  
+  };
+  return axios.post("/login", data)
+    .then(response => {
+      const jwt = response.token;
+      axios.defaults.headers.common.Authorization = `Bearer ${jwt}`;
+      return response;
+    })
+    .then((response) => {
+        console.log(response);
+        
+     return response;
+    })
+    .catch((error) => {
+    
+        return Promise.reject(error.response.data.error || error.response.data.message);
+    });
+}
+
 export function getAllUsers() {
+
+  const token =  localStorage.getItem("token")
   return axios
-    .get("/user")
+    .get("/user", { headers: {"authorization" : `Bearer ${token}`} })
     .then(function (response) {
       // handle success
 
@@ -36,34 +66,41 @@ export function registerUser(name, email, password, userType) {
     });
 }
 
-export function signInUser(email, password) {
-  const data = {
-  
-    email: email,
-    password: password,
-  
-  };
-  return axios.post("/login", data)
-    .then((response) => {
-        console.log(response);
-        
-     return response;
-    })
-    .catch((error) => {
-    
-        return Promise.reject(error.response.data.error || error.response.data.message);
-    });
-}
+
 
 export function deleteUser(id) {
   return axios
     .delete(`/user/${id}`)
     .then(function (response) {
       // handle success
+      return response;
     })
     .catch(function (error) {
       // handle error
       console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+}
+
+export function updateUser(id, userName, email, userType) {
+  const data = {
+    name: userName,
+    email: email,
+    userType: userType,
+  };
+  console.log(data)
+  return axios
+    .put(`/user/${id}`, data)
+    .then(function (response) {
+      console.log('res', response);
+      return Promise.resolve(response); 
+      // handle success
+    })
+    .catch(function (error) {
+      // handle error
+      return Promise.reject(error.response.data.error);
     })
     .finally(function () {
       // always executed
